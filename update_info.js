efd3edde55c78a26c678b72fab107ecabe43b1df
Christian Figueroa <figgchristian@gmail.com>
2016-11-01 21:40:02 -0700

    Added update_info js file
    Update info to display on main page and updates page

---
+++ update_info.js
@@ -0,0 +1,80 @@
+var UPDATES=[
+	{
+		title: 'New Timeline Page',
+		category: 'AP World History',
+		description: 'New timeline page that displays the years from 3,300 B.C.E. all the way to 2,100 C.E. and documents important events mentioned in the history textbook. It will be updated somewhat regularly as we read more chapters from the textbook.',
+		date: new Date(),
+		link:'./AP/History/Timeline'
+	},{
+		title: 'Chapter 15 Flash Cards',
+		category: 'AP World History',
+		description: '7 new flash cards: Sultanate of Delhi, Dhows, Junks, Kingdom of Axum, Shankara, Ramanuja, Guru Kabir.',
+		date: new Date('2016-10-31T06:02:43Z'),
+		link: './AP/History/Flash_Cards/find?t=1&q=14'
+	},{
+		title: 'Chapter 14 Flash Cards',
+		category: 'AP World History',
+		description: '10 new flash cards: Ka\'ba, Umma, Jihad, Caliph, Sufis, Baghdad, Dar Al-Islam, Jizya, Ulama, Madrases.',
+		date: new Date('2016-10-21T07:03:27Z'),
+		link: './AP/History/Flash_Cards/find?t=1&q=13'
+	},{
+		title: 'Draggable-Elements Library v1.0 Release',
+		category: 'JavaScript',
+		description: 'JavaScript library that allows for easy configuration of user-draggable elements. Click on me to see all the details.',
+		date: new Date('2016-09-25T10:13:53Z'),
+		link: './JavaScript/Draggable-Elements/home'
+	},{
+		title: 'Chapter 11 Flash Cards',
+		category: 'AP World History',
+		description: '10 new flash cards: Consuls, Latifundia, Patricians, Plebeians, Pax Romana, Tribunes, Twelve Tables, Punic Wars, Spartacus, Paterfamilias.',
+		date: new Date('2016-09-24T07:33:03Z'),
+		link: './AP/History/Flash_Cards/find?t=1&q=10'
+	},{
+		title: 'Chapter 10 Flash Cards',
+		category: 'AP World History',
+		description: '10 new flash cards: Alexander of Macedon, Aristotle, Hellenistic Era, Peloponnesian War, Skeptics, Athens, Sparta, Stoics, Homer, Olympic Games.',
+		date: new Date('2016-09-16T07:15:45Z'),
+		link: './AP/History/Flash_Cards/find?t=1&q=9'
+	},{
+		title: 'Chapter 9 Flash Cards',
+		category: 'AP World History',
+		description: '10 new flash cards: Ashoka, Bhagavad Gita, Dharma, Four Noble Truths, Jati, Krishna, Nirvana, Siddhartha Gautama, Vishnu, White Huns.',
+		date: new Date('2016-09-13T06:58:09Z'),
+		link: './AP/History/Flash_Cards/find?t=1&q=8'
+	},{
+		title: 'Chapter 8 Flash Cards',
+		category: 'AP World History',
+		description: '8 new flash cards: Analects, Laozi, Modu, Sericulture, Terracotta Army, Wuwei, Yellow Turban Uprising, Qin Shihuangdi.',
+		date: new Date('2016-09-05T17:33:23Z'),
+		link: './AP/History/Flash_Cards/find?t=1&q=7'
+	},{
+		title: 'Added Flash Cards up to Chapter 7',
+		category: 'AP World History',
+		description: 'Exactly as the title says. 31 total flash cards now.',
+		date: new Date('2016-08-26T06:00:45Z'),
+		link: './AP/History/Flash_Cards/'
+	},{
+		title: 'Created Vocabulary Flash Cards',
+		category: 'AP World History',
+		description: 'Set of pages to record all the vocabulary flash cards for each week. Will be updated each week we are assigned new words for a chapter.',
+		date: new Date('2016-08-22T07:09:04Z'),
+		link: './AP/History/Flash_Cards/'
+	},{
+		title: 'New LockHTML Theme: Flat Clock',
+		category: 'JavaScript',
+		description: 'Theme for jailbroken iphones. For use with LockHTML tweak.',
+		date: new Date('2016-08-04T06:51:08Z'),
+		link: './JavaScript/LockHTML/preview/?Flat_Clock'
+	},{
+		title: 'Created Scavenger Hunt Page',
+		category: 'AP Bio',
+		description: 'Summer project for AP Bio that includes 40 pictures of various items from a scavenger hunt list.',
+		date: new Date('2016-08-01T22:02:46Z'),
+		link: './AP/Biology/Scavenger-Hunt/'
+	}
+];
+var SVGS={
+	'AP World History': '<div class="updateicon svg-spartan"></div>',
+	'AP Bio': '<div class="updateicon svg-dna"></div>',
+	'JavaScript': '<div class="updateicon svg-js"></div>'
+};
