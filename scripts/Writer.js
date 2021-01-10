0c31b1b72f93f49696e0099be1be95c8aa3324bc
ChristianFigueroa <ChristianFigueroa@users.noreply.github.com>
2021-01-10 21:41:23 +0000

    deploy: 4fad56ea8b15160adfb6e7662c371b010a7a4b3a

--- scripts/Writer.js
+++ scripts/Writer.js
@@ -1,28 +1,36 @@
-window.Scheduler = window.Scheduler || (function() {
+"use strict";
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+
+window.Scheduler = window.Scheduler || (function () {
 	"use strict";
-	const REPEAT = "repeat";
-	const IF = "if";
-	const ELIF = "elif";
-	const ELSE = "else";
-	const IGNORE = "ignore";
+	var REPEAT = "repeat";
+	var IF = "if";
+	var ELIF = "elif";
+	var ELSE = "else";
+	var IGNORE = "ignore";
 
 	function event(type, callback, args) {
 		return {
 			type: type,
 			callback: callback,
 			args: args
-		}
+		};
 	}
 
-	let blocks = [];
+	var blocks = [];
 
 	function Scheduler() {
-		let makeEvent = (function(type, callback) {
-			return function(...args) {
+		var makeEvent = (function (type, callback) {
+			return (function () {
+				for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+					args[_key] = arguments[_key];
+				}
+
 				this.eventQueue.push(event(type, callback, args));
 				return this;
-			}.bind(this)
-		}).bind(this)
+			}).bind(this);
+		}).bind(this);
 
 		this.eventQueue = [];
 		this.eventIndex = 0;
@@ -30,42 +38,56 @@ window.Scheduler = window.Scheduler || (function() {
 
 		this.init = function init() {
 			if (blocks.length) {
-				let block = blocks.pop();
-				throw new Error("Unclosed .block." + block[0] + " block; use .block.end before using .init")
+				var block = blocks.pop();
+				throw new Error("Unclosed .block." + block[0] + " block; use .block.end before using .init");
 			}
 
 			if (this.isRunning = this.eventIndex < this.eventQueue.length) {
-				let event = this.eventQueue[this.eventIndex++];
-				event.callback.call(this, this.init.bind(this), ...event.args);
+				var _event$callback;
+
+				var _event = this.eventQueue[this.eventIndex++];
+				(_event$callback = _event.callback).call.apply(_event$callback, [this, this.init.bind(this)].concat(_toConsumableArray(_event.args)));
 			}
 			return this;
-		}
+		};
 
 		this.define = function newEvent(name, callback) {
-			this[name] = function(...args) {
+			this[name] = function () {
+				for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+					args[_key2] = arguments[_key2];
+				}
+
 				this.eventQueue.push(event(name, callback, args));
 				return this;
-			}
+			};
 			return this;
-		}
+		};
 
 		this.define("delay", function delay(resume, ms) {
 			setTimeout(resume, ms);
 		});
 
-		this.define("func", function func(resume, callback, ...args) {
-			callback.call(this, ...args);
+		this.define("func", function func(resume, callback) {
+			for (var _len3 = arguments.length, args = Array(_len3 > 2 ? _len3 - 2 : 0), _key3 = 2; _key3 < _len3; _key3++) {
+				args[_key3 - 2] = arguments[_key3];
+			}
+
+			callback.call.apply(callback, [this].concat(args));
 			resume();
 		});
 
-		this.define("funcasync", function funcasync(resume, callback, ...args) {
-			setTimeout(function() {
-				callback.call(this, resume, ...args);
-			}.bind(this), 0);
+		this.define("funcasync", function funcasync(resume, callback) {
+			for (var _len4 = arguments.length, args = Array(_len4 > 2 ? _len4 - 2 : 0), _key4 = 2; _key4 < _len4; _key4++) {
+				args[_key4 - 2] = arguments[_key4];
+			}
+
+			setTimeout((function () {
+				callback.call.apply(callback, [this, resume].concat(args));
+			}).bind(this), 0);
 		});
 
 		this.block = {
-			repeat: (function(n) {
+			repeat: (function (n) {
 				if (n == 0) {
 					blocks.push([IGNORE, this.eventQueue]);
 				} else {
@@ -73,12 +95,12 @@ window.Scheduler = window.Scheduler || (function() {
 				}
 				return this;
 			}).bind(this),
-			"if": (function(bool) {
+			"if": (function (bool) {
 				blocks.push([IF, this.eventQueue.length, bool]);
 				return this;
 			}).bind(this),
-			elif: (function(bool) {
-				let lastblock = blocks.pop();
+			elif: (function (bool) {
+				var lastblock = blocks.pop();
 				if (lastblock && (lastblock[0] == IF || lastblock[0] == ELIF)) {
 					if (lastblock[2]) {
 						blocks.push(lastblock, [ELIF, this.eventQueue.length, false]);
@@ -91,8 +113,8 @@ window.Scheduler = window.Scheduler || (function() {
 				}
 				return this;
 			}).bind(this),
-			"else": (function() {
-				let lastblock = blocks.pop();
+			"else": (function () {
+				var lastblock = blocks.pop();
 				if (lastblock && (lastblock[0] == IF || lastblock[0] == ELIF)) {
 					if (lastblock[2]) {
 						blocks.push(lastblock, [ELSE, this.eventQueue.length]);
@@ -105,12 +127,12 @@ window.Scheduler = window.Scheduler || (function() {
 				}
 				return this;
 			}).bind(this),
-			ignore: (function() {
+			ignore: (function () {
 				blocks.push([IGNORE, this.eventQueue.length]);
 				return this;
 			}).bind(this),
-			end: (function(bool) {
-				let lastblock = blocks.pop();
+			end: (function (bool) {
+				var lastblock = blocks.pop();
 				if (lastblock) {
 					if (lastblock[0] == IF) {
 						if (!lastblock[2]) {
@@ -120,8 +142,10 @@ window.Scheduler = window.Scheduler || (function() {
 						this.eventQueue.splice(lastblock[1]);
 						this.block.end();
 					} else if (lastblock[0] == REPEAT) {
-						for (let i = lastblock[2], events = this.eventQueue.slice(lastblock[1]); i >= 1; i--) {
-							this.eventQueue.push(...events);
+						for (var i = lastblock[2], events = this.eventQueue.slice(lastblock[1]); i >= 1; i--) {
+							var _eventQueue;
+
+							(_eventQueue = this.eventQueue).push.apply(_eventQueue, _toConsumableArray(events));
 						}
 					}
 				} else {
@@ -129,20 +153,20 @@ window.Scheduler = window.Scheduler || (function() {
 				}
 				return this;
 			}).bind(this)
-		}
+		};
 	}
 
 	return Scheduler;
 })();
 
-window.Writer = window.Writer || (function() {
+window.Writer = window.Writer || (function () {
 	"use strict";
-	const CARETTO = "caretto";
-	const END = "end";
-	const WRITE = "write";
-	const WRITEUNTIL = "writeuntil";
-	const RESTARTBLINKER = "restartblinker";
-	const REMOVE = "remove";
+	var CARETTO = "caretto";
+	var END = "end";
+	var WRITE = "write";
+	var WRITEUNTIL = "writeuntil";
+	var RESTARTBLINKER = "restartblinker";
+	var REMOVE = "remove";
 
 	function Writer() {
 		Scheduler.call(this);
@@ -151,7 +175,7 @@ window.Writer = window.Writer || (function() {
 		this.writingIndex = 0;
 		this.caretBlinker = null;
 
-		let startBlink = (function() {
+		var startBlink = (function () {
 			if (this.caretBlinker) {
 				stopBlink();
 			}
@@ -160,7 +184,7 @@ window.Writer = window.Writer || (function() {
 				this.writingElem.classList.add("caret");
 			}
 
-			this.caretBlinker = setInterval(function() {
+			this.caretBlinker = setInterval((function () {
 				if (!this.writingElem) {
 					stopBlink();
 					return;
@@ -171,10 +195,10 @@ window.Writer = window.Writer || (function() {
 				} else {
 					this.writingElem.classList.add("caret");
 				}
-			}.bind(this), 530);
+			}).bind(this), 530);
 		}).bind(this);
 
-		let stopBlink = (function() {
+		var stopBlink = (function () {
 			if (this.writingElem && this.caretBlinker) {
 				clearInterval(this.caretBlinker);
 				this.writingElem.classList.remove("caret");
@@ -198,10 +222,10 @@ window.Writer = window.Writer || (function() {
 			this.writingElem.classList.add("writing_elem");
 			this.writingIndex = 0;
 
-			let written = document.createElement("span");
-			let unwritten = document.createElement("span");
+			var written = document.createElement("span");
+			var unwritten = document.createElement("span");
 
-			written.innerText = "\u200b";
+			written.innerText = "​";
 
 			unwritten.innerText = elem.innerText;
 			unwritten.style.color = "transparent";
@@ -224,42 +248,42 @@ window.Writer = window.Writer || (function() {
 			stopBlink();
 			this.writingElem = null;
 			resume();
-		})
+		});
 
-		this.define(WRITE, function(resume, time, numchars) {
+		this.define(WRITE, function (resume, time, numchars) {
 			stopBlink();
-			let elem = this.writingElem;
-			let text = elem.innerText.replace(/\n/g, "").replace(/^\u200b/ug, "").replace(/\u00a0/g, " ");
-			let index = this.writingIndex || 0;
+			var elem = this.writingElem;
+			var text = elem.innerText.replace(/\n/g, "").replace(/^\u200B/g, "").replace(/\u00a0/g, " ");
+			var index = this.writingIndex || 0;
 
 			if (typeof numchars != "number") {
 				numchars = Infinity;
 			}
 			numchars = Math.max(Math.min(text.length - index, numchars), 0);
 
-			let untilIndex = index + numchars;
+			var untilIndex = index + numchars;
 
 			elem.classList.add("writing");
 
-			let written = elem.firstElementChild;
-			let unwritten = elem.lastElementChild;
+			var written = elem.firstElementChild;
+			var unwritten = elem.lastElementChild;
 
 			if (written.lastChild && written.lastChild.nodeType == document.TEXT_NODE) {
-				written.lastChild.data = written.lastChild.data.replace(/^\u200b/u, "");
+				written.lastChild.data = written.lastChild.data.replace(/^\u200B/, "");
 			}
 
-			let interval = setInterval(function() {
+			var interval = setInterval((function () {
 				if (++index <= untilIndex) {
 					this.writingIndex = index;
-					let char = text[index - 1];
+					var char = text[index - 1];
 
 					if (written.lastChild && written.lastChild.nodeType == document.TEXT_NODE) {
-						written.lastChild.data = written.lastChild.data.replace(/\u00a0$/, " ") + (char == " " ? "\u00a0" : char);
+						written.lastChild.data = written.lastChild.data.replace(/\u00a0$/, " ") + (char == " " ? " " : char);
 					} else {
-						written.appendChild(document.createTextNode(char == " " ? "\u00a0" : char));
+						written.appendChild(document.createTextNode(char == " " ? " " : char));
 					}
 
-					unwritten.innerText = text.substring(index).replace(/ /g, "\u00a0");
+					unwritten.innerText = text.substring(index).replace(/ /g, " ");
 
 					if (/\W/.test(char) && char != " ") {
 						written.appendChild(document.createElement("wbr"));
@@ -280,10 +304,10 @@ window.Writer = window.Writer || (function() {
 					startBlink();
 					resume();
 				}
-			}.bind(this), time ? time : 50);
+			}).bind(this), time ? time : 50);
 		});
 
-		this.define(REMOVE, function(resume) {
+		this.define(REMOVE, function (resume) {
 			if (this.writingElem) {
 				this.writingElem.parentNode.removeChild(this.writingElem);
 				stopBlink();
